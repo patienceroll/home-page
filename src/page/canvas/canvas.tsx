bdcd873525cba129c8be4601d06c4203b36f0bff
pactience <1079105171@qq.com>
2021-03-08 17:42:43 +0800

    feat(修改文案): 修改各个页面展示的文案

--- src/page/canvas/canvas.tsx
+++ src/page/canvas/canvas.tsx
@@ -5,7 +5,7 @@ import Clock from "@src/componets-canvas/clock/clock";
 const Canvas: FC = memo(() => {
     return (
         <div style={{ height: "100%" }}>
-            canvas页面内
+            canvas页面
             <Clock style={{height:'100%'}} />
         </div>
     );
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -5,7 +5,7 @@ import Loading from "@src/componets-canvas/loading/loading";
 const Home: React.FC = memo(() => {
     return (
         <div>
-            <div style={{ textAlign: "center" }}>home页面内</div>
+            <div style={{ textAlign: "center" }}>home页面</div>
             <Loading />
         </div>
     );
