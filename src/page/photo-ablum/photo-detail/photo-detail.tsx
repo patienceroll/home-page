fdac20d0bb8716862439e0aaa6a26f9ea4fb57d1
pactience <1079105171@qq.com>
2021-04-07 10:56:25 +0800

    feat: 🎸 开始相册详情页

--- src/page/photo-ablum/photo-detail/photo-detail.tsx
+++ src/page/photo-ablum/photo-detail/photo-detail.tsx
@@ -1,9 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, memo, useCallback, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const PhotoDetail: FC = () => {
-    return <div>
-        1231223213
-    </div>;
-};
+import Loading from "@src/componets-canvas/loading/loading";
+
+import * as Data from "../data";
+
+import * as Request from "../service";
+
+import Style from "./photo-detail.module.less";
+
+const PhotoDetail: FC = memo(() => {
+    const { id } = useParams<{ id: string }>();
+    const [detail, setDetail] = useState<Data.PhotoDetail>();
+
+    const getDetail = useCallback(() => {
+        setDetail(undefined);
+        Request.GetPhoto({ id }).then(res => {
+            setDetail(res.data);
+        });
+    }, [id]);
+
+    useEffect(getDetail, [getDetail]);
+
+    return detail ? (
+        <div className={Style.CT}>
+            <div dangerouslySetInnerHTML={{ __html: decodeURIComponent(detail.content) }} />
+        </div>
+    ) : (
+        <Loading />
+    );
+});
 
 export default PhotoDetail;
